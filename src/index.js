e8a4a1df89a4a1084ff72808b7ec908f71f43f99
Nachiket-N <nachiket.n.naik@gmail.com>
2021-07-11 22:17:45 +0530

    import change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
-const userRouter = require("../src/routers/user.js");
-const taskRouter = require("../src/routers/task.js");
+const userRouter = require("../src/routers/User.js");
+const taskRouter = require("../src/routers/Task.js");
 
 const app = express();
 const port = process.env.PORT;
